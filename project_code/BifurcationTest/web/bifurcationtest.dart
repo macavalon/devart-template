7b03506c846cf58e802e397a73ab8f13a29caffd
macavalon <macavalon@googlemail.com>
2014-03-28 00:15:26 +0000

    the end product

--- project_code/BifurcationTest/web/bifurcationtest.dart
+++ project_code/BifurcationTest/web/bifurcationtest.dart
@@ -6,6 +6,7 @@ library bifurcation;
 
 import 'dart:html';
 import 'dart:math';
+import 'dart:async';
 
 const String ORANGE = "orange";
 const int SEED_RADIUS = 1;
@@ -19,6 +20,7 @@ const int MAX_Y_PLOT = 520;
 ///double r = 2.0; /// bifurcation parameter
 num initial_r =2.0;
 num end_r = 4.0;
+num seed_value = 0.5;
 
 //const num centerX = MAX_D / 2;
 //const num centerY = centerX;
@@ -26,24 +28,32 @@ num end_r = 4.0;
 final InputElement iterations_slider = querySelector("#iterations_slider");
 final InputElement r_start_slider = querySelector("#r_start_slider");
 final InputElement r_end_slider = querySelector("#r_end_slider");
+final InputElement r_seed_slider = querySelector("#r_seed_slider");
 
 final Element notes = querySelector("#notes");
 final Element notes1 = querySelector("#notes1");
 final Element notes2 = querySelector("#notes2");
+final Element notes3 = querySelector("#notes3");
 final num PHI = (sqrt(5) + 1) / 2;
 
 final CanvasRenderingContext2D context =
   (querySelector("#canvas") as CanvasElement).context2D;
 
 void main() {
-  iterations_slider.onChange.listen((e) => drawLyapunov());
-  r_start_slider.onChange.listen((e) => drawLyapunov());
-  r_end_slider.onChange.listen((e) => drawLyapunov());
-  drawLyapunov();
+  iterations_slider.onChange.listen((e) => drawLyapunov(0.0));
+  r_start_slider.onChange.listen((e) => drawLyapunov(0.0));
+  r_end_slider.onChange.listen((e) => drawLyapunov(0.0));
+  r_seed_slider.onChange.listen((e) => drawLyapunov(0.0));
+
+    drawLyapunov(0.0);
+}
+
+Future sleep1() {
+  return new Future.delayed(const Duration(seconds: 1), () => "1");
 }
 
 /// Draw the complete figure for the current number of seeds.
-void drawLyapunov() {
+void drawLyapunov(num seedOverride) {
 
   int xmax = MAX_X;
   int ymax = MAX_Y;
@@ -60,15 +70,25 @@ void drawLyapunov() {
   num yplot = 0.0;
   
   
-  
-  
-  
-  num seed_value = 0.5;
-  
 
   int iterations = num.parse(iterations_slider.value);
   initial_r = num.parse(r_start_slider.value);
   end_r = num.parse(r_end_slider.value);
+  if(seedOverride==0.0)
+  {
+    seed_value = num.parse(r_seed_slider.value);
+  }
+  else
+  {
+    seed_value = seedOverride;  
+  
+  }
+  if(initial_r >= end_r)
+  {
+    //assume user wants very close ?
+    end_r = initial_r+0.05;
+    
+  }
   
   num r = initial_r;
   
@@ -104,7 +124,7 @@ void drawLyapunov() {
     // calculate lyapunov exponent
    for (j = 0; j < iterations; j++)
    {
-      x = r*x*(1.0-x); 
+     x = r*x*(1.0-x); 
       xeps = x-eps;
        
        xeps = r*xeps*(1.0-xeps);
@@ -112,7 +132,7 @@ void drawLyapunov() {
        sum += log(distance/eps);
    }
 
-   num lambda = (sum/iterations);/**50;*/
+  num lambda = (sum/iterations);/**50;*/
  
    
    // encode the colours
@@ -124,13 +144,13 @@ void drawLyapunov() {
    
    cr = (255 -lambdafloor.abs()).clamp(0, 255);
    cg = (lambdaremainderGreen).clamp(0, 255).floor();
-   cb = (lambdaremainderBlue).clamp(0, 255).floor();
+   //cb = (lambdaremainderBlue).clamp(0, 255).floor();
    
    cb = 0;
 
    
    
-   for (k = 0; k < iterations; k++)
+   for (k = 1; k < iterations; k++)
     {
        x = r*x*(1.0-x); 
        
@@ -141,7 +161,23 @@ void drawLyapunov() {
        n = yplot.floor();
        
        // colour is set by lyapunov exponent
-       
+       //xeps = x-eps;
+              
+       // xeps = r*xeps*(1.0-xeps);
+       // num distance = (x-xeps).abs();
+      //  sum += log(distance/eps);
+       // num lambda = (sum/k);
+        
+       // num lambdafloor = lambda.floor();
+       //  num lambdaremainderGreen = (lambda - lambdafloor)*255;
+       //  num lambdaremainderGreenfloor = lambdaremainderGreen.floor();
+      //   num lambdaremainderBlue = (lambdaremainderGreen-lambdaremainderGreenfloor)*255;
+         
+      //   cr = (255 -lambdafloor.abs()).clamp(0, 255);
+      //   cg = (255-lambdaremainderGreen).clamp(0, 255).floor();
+      //   cb = (255-lambdaremainderBlue).clamp(0, 255).floor();
+         
+         //cb = 0;
        
        drawPixel(m,n,cr,cg,cb);
     }
@@ -156,6 +192,7 @@ void drawLyapunov() {
   notes.text = "bifurcation iterations : ${iterations} ";
   notes1.text = "rStart : ${initial_r}";
   notes2.text = " rEnd : ${end_r}";
+  notes3.text = " rSeed : ${seed_value}";
   
   drawXaxisLabels(1,2);
     
@@ -165,7 +202,7 @@ void drawLyapunov() {
 /// Draw a small circle representing a seed centered at (x,y).
 void drawPixel(num x, num y, int r, int g, int b) {
   context..beginPath()
-         ..lineWidth = 2
+         ..lineWidth = 1
          ..setStrokeColorRgb(r,g,b,1.0)
          ..strokeRect(x,y,SEED_RADIUS,SEED_RADIUS)
          ..fill()
